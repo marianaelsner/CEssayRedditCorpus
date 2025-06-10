ef5d5f5dff12f72aba7ece1fe799dcad8b385936
marianaelsner <31534993+marianaelsner@users.noreply.github.com>
2025-06-10 18:38:21 +0200

    Update and rename 2.ProcessingSteps/step2 wordcount.py to 2.ProcessingSteps/AuxiliaryFiles/wordcount.py

--- 2.ProcessingSteps/AuxiliaryFiles/wordcount.py
+++ 2.ProcessingSteps/AuxiliaryFiles/wordcount.py
@@ -6,7 +6,7 @@ from concurrent.futures import ThreadPoolExecutor
 print("*" * 150)
 print("Starting Step 2")
 
-csv_file = r'C:/Users/Mariana/Documents/Python/csv/Data_1_Done.csv'
+csv_file = r'C:/Users/Mariana/Documents/Python/csv/Data_0_Done.csv'
 
 print(F"Reading CSV {csv_file}")
 
@@ -25,7 +25,7 @@ print(stats)
 stats.to_csv(r'steps\Step2WordCount.csv')
 
 ## We save the stuff here
-df_output_file = r'C:/Users/Mariana/Documents/Python/csv/Data_2_Done.csv'
+df_output_file = r'C:/Users/Mariana/Documents/Python/csv/Data_0_Done.csv'
 
 df.to_csv(df_output_file, encoding='utf-8', index=False)
 print(f"Combined DataFrame saved to {df_output_file}")
