a2f85bf291e497c8045619f25f6ff7d76548c91d
marianaelsner <31534993+marianaelsner@users.noreply.github.com>
2025-06-10 19:11:14 +0200

    Update startThreads.ps1

--- 2.ProcessingSteps/MainProcessing/Step1/startThreads.ps1
+++ 2.ProcessingSteps/MainProcessing/Step1/startThreads.ps1
@@ -2,5 +2,5 @@
 $threads = 29
 for ($i = 0; $i -lt $threads; $i++) {
     Write-Host "Starting thread $i"
-    Start-Process powershell -ArgumentList "-NoExit", "-Command", "C:\Users\Mariana\Documents\Python\.venv\Scripts\Activate.ps1; Python C:\Users\Mariana\Documents\Python\steps\word_embedding\Step1\start_thread.py $i"
+    Start-Process powershell -ArgumentList "-NoExit", "-Command", "C:\Users\Mariana\Documents\Python\.venv\Scripts\Activate.ps1; Python C:\Users\Mariana\Documents\Python\steps\step1\start_thread.py $i"
 }
